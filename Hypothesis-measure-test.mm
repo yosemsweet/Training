e1d3de3cc48903fc754f8a7701614c0084254aa7
Yosem Sweet <yosem.sweet@suite101.com>
2011-03-10 14:58:51 -0800

    Added business modelling training

---
+++ Hypothesis-measure-test.mm
@@ -0,0 +1,19 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1299472675487" ID="Freemind_Link_616341595" MODIFIED="1299472735934" TEXT="Reader">
+<node CREATED="1299473012404" ID="Freemind_Link_411018820" MODIFIED="1299473025288" POSITION="left" TEXT="Hypothesis">
+<node CREATED="1299472736827" ID="_" MODIFIED="1299475771348" TEXT="Our primary customer">
+<node CREATED="1299472982005" ID="Freemind_Link_474083844" MODIFIED="1299472989576" TEXT="Why do we think that&apos;s our primary customer?"/>
+<node CREATED="1299472746371" ID="Freemind_Link_522949338" MODIFIED="1299472979441" TEXT="What are the major problems we solve for them?"/>
+<node CREATED="1299473001013" ID="Freemind_Link_132241525" MODIFIED="1299473054383" TEXT="How do we reach them?"/>
+</node>
+</node>
+<node CREATED="1299473026093" ID="Freemind_Link_989652983" MODIFIED="1299475654690" POSITION="left" TEXT="What metric to measure">
+<node CREATED="1299473329870" ID="Freemind_Link_1531692903" MODIFIED="1299475878344" TEXT="Positive/negative reaction"/>
+<node CREATED="1299475808533" ID="Freemind_Link_941216973" MODIFIED="1299475905054" TEXT="Helpful range?"/>
+<node CREATED="1299475905690" ID="Freemind_Link_1565520554" MODIFIED="1299475910454" TEXT="Would you recommend this?"/>
+<node CREATED="1299473381236" ID="Freemind_Link_1873391475" MODIFIED="1299475919711" TEXT="Would you pay for this?"/>
+</node>
+<node CREATED="1299473029420" ID="Freemind_Link_751998624" MODIFIED="1299475657972" POSITION="left" TEXT="Test"/>
+</node>
+</map>
